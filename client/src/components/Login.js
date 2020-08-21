da50abbe815f3b6f67b203f57eef691caa47196d
jacksonet00 <jacksoneliastaylor@gmail.com>
2020-08-21 13:15:32 -0400

    initialize test data

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -3,12 +3,9 @@ import { withRouter, Redirect } from 'react-router';
 import { NavLink } from 'react-router-dom';
 import app from '../firebase';
 import { AuthContext } from '../Auth';
-import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
@@ -41,7 +38,7 @@ const Copyright = () => {
 		<Typography variant="body2" color="textSecondary" align="center">
 			{'Copyright © '}
 			<Link color="inherit" href="https://material-ui.com/">
-				Your Website
+				Sawgrass Nutralabs
 			</Link>{' '}
 			{new Date().getFullYear()}
 			{'.'}
@@ -80,7 +77,6 @@ const Login = ({ history }) => {
 		<Container component="main" maxWidth="xs">
 			<CssBaseline />
 			<div className={classes.paper}>
-				<Avatar className={classes.avatar}></Avatar>
 				<Typography component="h1" variant="h5">
 					Sign in
 				</Typography>
@@ -106,12 +102,6 @@ const Login = ({ history }) => {
 						type="password"
 						id="password"
 						autoComplete="current-password"
-					/>
-					<FormControlLabel
-						control={
-							<Checkbox value="remember" color="primary" />
-						}
-						label="Remember me"
 					/>
 					<Button
 						type="submit"

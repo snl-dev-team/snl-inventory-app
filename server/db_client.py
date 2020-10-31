61df0776dc0cfa7a7297d9c49e520f62895af9b8
Isaiah Nields <imnields@gmail.com>
2020-10-31 18:03:32 -0400

    Add pyhton deploy

---
+++ server/db_client.py
@@ -0,0 +1,21 @@
+import mysql.connector
+import sys
+import boto3
+import os
+import json
+
+rds_client = boto3.client('rds-data')
+database_secrets_arn = os.environ.get('DATABASE_SECRETS_ARN')
+database_name = os.environ.get('DATABASE_NAME')
+db_cluster_arn = os.environ.get('DATABASE_CLUSTER_ARN')
+
+
+def execute_statement(sql):
+    print(sql)
+    response = rds_client.execute_statement(
+        secretArn=database_secrets_arn,
+        database=database_name,
+        resourceArn=db_cluster_arn,
+        sql=sql
+    )
+    return response
---
+++ server/material.test.py
@@ -0,0 +1,23 @@
+import unittest
+import material
+import os
+import json
+
+
+with open('./database_credentials.json') as f:
+    db_credentials = json.load(f)
+
+
+class TestMaterial(unittest.TestCase):
+
+    def test_fetch(self):
+        material.fetch(None, None)
+
+    def setUp(self):
+        os.environ['DATABASE_SECRETS_ARN'] = db_credentials['databaseSecretsArn']
+        os.environ['DATABASE_NAME'] = db_credentials['databaseClusterArn']
+        os.environ['DATABASE_CLUSTER_ARN'] = db_credentials['databaseName']
+
+
+if __name__ == '__main__':
+    unittest.main()

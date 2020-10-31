88fd9667aa0a685a06258b2669eb226f69e6632c
Isaiah Nields <imnields@gmail.com>
2020-10-31 12:30:03 -0400

    Update index.js

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,7 +1,7 @@
 import { applyMiddleware, createStore } from 'redux';
-import rootReducer from '../reducers/index';
-import promise from 'redux-promise-middleware'
+import promise from 'redux-promise-middleware';
 import logger from 'redux-logger';
+import rootReducer from '../reducers/index';
 
 const store = createStore(rootReducer, applyMiddleware(logger, promise));
 export default store;

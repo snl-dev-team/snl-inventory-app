165fa4f68170f4241f05641571264b86cc447947
Isaiah Nields <imnields@gmail.com>
2020-11-01 09:30:46 -0500

    Add fetch products

--- server/product.py
+++ server/product.py
@@ -88,3 +88,44 @@ def createProduct(event, context):
             'body': str(e),
             'headers': headers
         }
+
+
+def fetchProducts(event, context):
+
+    headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Request-Method': 'GET',
+        'Access-Control-Allow-Headers': 'content-type'
+    }
+
+    try:
+        columns_string = ', '.join(i[0] for i in COLUMNS)
+        sql = """
+        SELECT
+            {columns}
+        FROM
+            `material`;
+        """.format(columns=columns_string)
+        res = execute_statement(sql)
+        records = res['records']
+        data = []
+        for record in records:
+            data_row = {}
+            for entry, column in zip(record, COLUMNS):
+                name, _, entry_type = column
+                value = entry.get(entry_type, None)
+                data_row[name] = value
+            data.append(data_row)
+
+        return {
+            'statusCode': 200,
+            'body': json.dumps({'data': data}),
+            'headers': headers
+        }
+
+    except Exception as e:
+        return {
+            'statusCode': 400,
+            'body': json.dumps({'message': str(e)}),
+            'headers': headers
+        }

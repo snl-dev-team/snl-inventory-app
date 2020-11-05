b7e6d8b758021e24e2e57a93d7fdd10f57bf28b0
jacksonet00 <jacksoneliastaylor@gmail.com>
2020-11-05 15:59:31 -0500

    Add unuseMaterial reducer

--- client/src/reducers/productUseMaterial.js
+++ client/src/reducers/productUseMaterial.js
@@ -1,4 +1,4 @@
-import { USE_MATERIAL } from '../actions/productUseMaterial';
+import { UNUSE_MATERIAL, USE_MATERIAL } from '../actions/productUseMaterial';
 
 const productUseMaterialReducer = (state = {}, action) => {
 	const { type, payload, meta } = action;
@@ -8,7 +8,19 @@ const productUseMaterialReducer = (state = {}, action) => {
 			return {
 				...state,
 				...payload.reduce((acc, curr) => {
-					acc[curr.id] = {
+					acc[curr.product_id] = {
+						productId: curr.product_id,
+						materialId: curr.material_id,
+						count: curr.count,
+					};
+					return acc;
+				}, {}),
+			};
+		case `${UNUSE_MATERIAL}_FULFILLED`:
+			return {
+				...state,
+				...payload.reduce((acc, curr) => {
+					acc[curr.product_id] = {
 						productId: curr.product_id,
 						materialId: curr.material_id,
 						count: curr.count,

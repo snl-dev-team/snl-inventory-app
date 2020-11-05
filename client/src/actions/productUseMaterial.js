e6381f838872d3e914a792efaf1db1e4de3061c7
jacksonet00 <jacksoneliastaylor@gmail.com>
2020-11-05 15:57:28 -0500

    Add unuseMaterial action

--- client/src/actions/productUseMaterial.js
+++ client/src/actions/productUseMaterial.js
@@ -1,11 +1,31 @@
 import axios from 'axios';
 
 export const USE_MATERIAL = 'USE_MATERIAL';
+export const UNUSE_MATERIAL = 'UNUSE_MATERIAL';
 
 export const useMaterial = ({ productId, materialId, count }) => ({
 	type: USE_MATERIAL,
 	payload: axios
 		.post(
+			`https://rqzpcqdt40.execute-api.us-east-1.amazonaws.com/dev//product/${productId}/material`,
+			{
+				product_id: productId,
+				material_id: materialId,
+				count: count,
+			},
+			{
+				headers: {
+					'content-type': 'application/json',
+				},
+			}
+		)
+		.then((res) => res.data),
+});
+
+export const unuseMaterial = ({ productId, materialId, count }) => ({
+	type: UNUSE_MATERIAL,
+	payload: axios
+		.delete(
 			`https://rqzpcqdt40.execute-api.us-east-1.amazonaws.com/dev//product/${productId}/material`,
 			{
 				product_id: productId,

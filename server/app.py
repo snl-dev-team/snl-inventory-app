d45392ebd10c4df3ce09176ac03d188b51579ff7
jacksonet00 <jacksoneliastaylor@gmail.com>
2020-11-05 15:14:12 -0500

    material table updates with product_use_material table

--- server/app.py
+++ server/app.py
@@ -365,6 +365,8 @@ def product_use_material(id):
         body = app.current_request.json_body
 
         sql = """
+            UPDATE `material` SET `count`=((SELECT `count` FROM `material` WHERE `id`={material_id}) - {count}) WHERE `id`={material_id};
+
             REPLACE INTO `product_uses_material` (
                 `product_id`,
                 `material_id`,
@@ -374,7 +376,7 @@ def product_use_material(id):
                 {id},
                 {material_id},
                 {count}
-            )
+            );
             """.format(**body, id=id)
 
         execute_statement(sql)

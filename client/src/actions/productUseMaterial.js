36f5bd3857143ae25b99b392b163903d3ef64e1d
jacksonet00 <jacksoneliastaylor@gmail.com>
2020-11-05 17:26:14 -0500

    Modify useMaterial

--- client/src/actions/productUseMaterial.js
+++ client/src/actions/productUseMaterial.js
@@ -6,7 +6,7 @@ export const UNUSE_MATERIAL = 'UNUSE_MATERIAL';
 export const useMaterial = ({ productId, materialId, count }) => ({
 	type: USE_MATERIAL,
 	payload: axios
-		.post(
+		.put(
 			`https://rqzpcqdt40.execute-api.us-east-1.amazonaws.com/dev//product/${productId}/material`,
 			{
 				product_id: productId,

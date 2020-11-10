34f2e46e0669d170685a90814e9dce3a6545d209
Isaiah Nields <imnields@gmail.com>
2020-11-10 08:54:41 -0500

    Fix linting

--- client/src/reducers/order.js
+++ client/src/reducers/order.js
@@ -1,75 +1,76 @@
 import {
-	FETCH_ORDERS,
-	CREATE_ORDER,
-	UPDATE_ORDER,
-	DELETE_ORDER,
+  FETCH_ORDERS,
+  FETCH_ORDER,
+  CREATE_ORDER,
+  UPDATE_ORDER,
+  DELETE_ORDER,
 } from '../actions/order';
 
 const orderReducer = (state = {}, action) => {
-	const { type, payload, meta } = action;
+  const { type, payload, meta } = action;
 
-	switch (type) {
-		case `${FETCH_ORDERS}_FULFILLED`:
-			return {
-				...state,
-				...payload.reduce((acc, curr) => {
-					acc[curr.id] = {
-						id: curr.id,
-						number: curr.number,
-						dateCreated: curr.date_created,
-						dateModified: curr.date_modified,
-					};
-					return acc;
-				}, {}),
-			};
-		case `${FETCH_ORDERS}_FULFILLED`:
-			return {
-				...state,
-				[payload.id]: {
-					id: payload.id,
-					number: payload.number,
-					dateCreated: payload.date_created,
-					dateModified: payload.date_modified,
-				},
-			};
+  switch (type) {
+    case `${FETCH_ORDERS}_FULFILLED`:
+      return {
+        ...state,
+        ...payload.reduce((acc, curr) => {
+          acc[curr.id] = {
+            id: curr.id,
+            number: curr.number,
+            dateCreated: curr.date_created,
+            dateModified: curr.date_modified,
+          };
+          return acc;
+        }, {}),
+      };
+    case `${FETCH_ORDER}_FULFILLED`:
+      return {
+        ...state,
+        [payload.id]: {
+          id: payload.id,
+          number: payload.number,
+          dateCreated: payload.date_created,
+          dateModified: payload.date_modified,
+        },
+      };
 
-		case `${CREATE_ORDER}_FULFILLED`: {
-			const { order } = meta;
+    case `${CREATE_ORDER}_FULFILLED`: {
+      const { order } = meta;
 
-			return {
-				...state,
-				[payload.id]: {
-					...order,
-					id: payload.id,
-					dateCreated: payload.date_created,
-					dateModified: payload.date_modified,
-				},
-			};
-		}
+      return {
+        ...state,
+        [payload.id]: {
+          ...order,
+          id: payload.id,
+          dateCreated: payload.date_created,
+          dateModified: payload.date_modified,
+        },
+      };
+    }
 
-		case `${UPDATE_ORDER}_FULFILLED`: {
-			const { order } = meta;
+    case `${UPDATE_ORDER}_FULFILLED`: {
+      const { order } = meta;
 
-			return {
-				...state,
-				[order.id]: {
-					...state[order.id],
-					...order,
-					dateModified: payload.date_modified,
-				},
-			};
-		}
+      return {
+        ...state,
+        [order.id]: {
+          ...state[order.id],
+          ...order,
+          dateModified: payload.date_modified,
+        },
+      };
+    }
 
-		case `${DELETE_ORDER}_FULFILLED`: {
-			const { id } = meta;
-			const deleteState = Object.assign(state);
-			delete deleteState[id];
-			return deleteState;
-		}
+    case `${DELETE_ORDER}_FULFILLED`: {
+      const { id } = meta;
+      const deleteState = Object.assign(state);
+      delete deleteState[id];
+      return deleteState;
+    }
 
-		default:
-			return state;
-	}
+    default:
+      return state;
+  }
 };
 
 export default orderReducer;

6172a783d267bc0227346398ccbc4ac8987f3a90
jacksonet00 <jacksoneliastaylor@gmail.com>
2020-11-23 09:39:03 -0500

    add case actions

--- client/src/constants/caseActionTypes.js
+++ client/src/constants/caseActionTypes.js
@@ -1,5 +1,16 @@
+// case
 export const CREATE_CASE = 'CREATE_CASE';
 export const FETCH_CASES = 'FETCH_CASES';
 export const FETCH_CASE = 'FETCH_CASE';
 export const UPDATE_CASE = 'UPDATE_CASE';
 export const DELETE_CASE = 'DELETE_CASE';
+
+// case_uses_material
+export const FETCH_CASE_USES_MATERIAL = '';
+export const CASE_USE_MATERIAL = '';
+export const CASE_UNUSE_MATERIAL = '';
+
+// case _uses_product
+export const FETCH_CASE_USES_PRODUCT = '';
+export const CASE_USE_PRODUCT = '';
+export const CASE_UNUSE_PRODUCT = '';

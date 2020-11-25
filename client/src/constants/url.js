e39bde48b65fe01603c7edc4fd3bdddb6355015c
Isaiah Nields <imnields@gmail.com>
2020-11-25 02:47:24 -0500

    Update url.js

--- client/src/constants/url.js
+++ client/src/constants/url.js
@@ -1,4 +1,4 @@
 export const DEV = 'http://localhost:8001';
-export const PROD = 'https://f575f737c8.execute-api.us-east-1.amazonaws.com/dev/';
+export const PROD = 'https://f575f737c8.execute-api.us-east-1.amazonaws.com/dev';
 
 export const URL = process.env.NODE_ENV === 'development' ? DEV : PROD;

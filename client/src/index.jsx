6b16030e209f519b3c929a27c33c23e1f688fb94
Isaiah Nields <imnields@gmail.com>
2020-11-24 19:46:26 -0500

    Add Apollo Client

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,15 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import App from './App';
 import { store, persistor } from './store';
+import { URL } from './constants/url';
+
+const client = new ApolloClient({
+  uri: `${URL}/graphql`,
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
-  <Provider store={store}>
-    <PersistGate persistor={persistor}>
-      <App />
-    </PersistGate>
-  </Provider>,
+  <ApolloProvider client={client}>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </ApolloProvider>,
   // eslint-disable-next-line no-undef
   document.getElementById('root'),
 );

0761d0cc557e7cef4d97fd1ea5a2f971692628cf
Isaiah Nields <imnields@gmail.com>
2020-11-28 10:57:13 -0500

    update theming

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import GraphiQL from 'graphiql';
 import fetch from 'isomorphic-fetch';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import Dashboard from './containers/dashboard/Dashboard';
 import SignUp from './containers/authentication/SignUp';
 import SignUpConfirm from './containers/authentication/SignUpConfirm';
@@ -18,19 +19,31 @@ function graphQLFetcher(graphQLParams) {
     body: JSON.stringify(graphQLParams),
   }).then((response) => response.json());
 }
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#1c541c',
+    },
+    secondary: {
+      main: '#74b820',
+    },
+  },
+});
 
 const App = () => (
-  <Router>
-    <Switch>
-      <Route exact path="/sign-up" component={SignUp} />
-      <Route exact path="/sign-up/confirm" component={SignUpConfirm} />
-      <Route exact path="/forgot-password" component={ForgotPassword} />
-      <Route exact path="/forgot-password/submit" component={ForgotPasswordSubmit} />
-      <Route exact path="/sign-in" component={SignIn} />
-      <Route exact path="/graphql" component={() => <div style={{ height: '900px' }}><GraphiQL fetcher={graphQLFetcher} /></div>} />
-      <Route path="/" component={Dashboard} />
-    </Switch>
-  </Router>
+  <ThemeProvider theme={theme}>
+    <Router>
+      <Switch>
+        <Route exact path="/sign-up" component={SignUp} />
+        <Route exact path="/sign-up/confirm" component={SignUpConfirm} />
+        <Route exact path="/forgot-password" component={ForgotPassword} />
+        <Route exact path="/forgot-password/submit" component={ForgotPasswordSubmit} />
+        <Route exact path="/sign-in" component={SignIn} />
+        <Route exact path="/graphql" component={() => <div style={{ height: '900px' }}><GraphiQL fetcher={graphQLFetcher} /></div>} />
+        <Route path="/" component={Dashboard} />
+      </Switch>
+    </Router>
+  </ThemeProvider>
 );
 
 export default App;

d44c4d26f68f3513c7a0e38cde86b0edf3dc78ad
Isaiah Nields <imnields@gmail.com>
2020-12-04 21:53:26 -0500

    Add create user.

---
+++ client/src/constants/email.js
@@ -0,0 +1,1 @@
+export default 'mailto:imnields@gmail.com?subject=SNL%20Inventory%20Tracker%20Create%20User&body=Hey%20Isaiah!%0D%0A%0D%0AJust%20wanted%20to%20let%20you%20know%20that%20you%20are%20sexy%20and%20awesome.%0D%0A%0D%0AAlso%2C%20I%20would%20like%20to%20sign%20up%20%3CINSERT_EMAIL%3E%20to%20use%20the%20SNL%20Inventory%20Tracker.%20Would%20you%20mind%20setting%20this%20up%3F';
